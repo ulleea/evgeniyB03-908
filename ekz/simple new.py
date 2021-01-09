f69c949a856e781934c2ffaaf8751dcdbd3e76d6
123 <123>
2021-01-09 14:26:12 +0300

    async with another threads

--- ekz/simple new.py
+++ ekz/simple new.py
@@ -1,6 +1,6 @@
-def simple(n=1,):
-    global a
-    while a:
+def simple():
+    global a,n
+    if a:
         m = False
         k = 0
         for j in range(1, n + 1):
@@ -12,9 +12,6 @@ def simple(n=1,):
         if k == 0:
             if m:
                 print(m)
-                thread2 = threading.Thread(target=stopper, args=())
-                thread2.start()
-                thread2.join(2.0)
         n+=1
 
 def stopper():
@@ -22,10 +19,21 @@ def stopper():
     s=input()
     if s=='stop':
         a=False
-        print(123)
     else:
         a=True
+
+def runner():
+    while a:
+        thread1 = threading.Thread(target=simple, args=())
+        thread1.start()
+        thread1.join()
+
+        thread2 = threading.Thread(target=stopper, args=())
+        thread2.start()
+        thread2.join(2.0)
 import threading
 a=True
-thread1=threading.Thread(target=simple,args=())
-thread1.start()
+n=1
+runner()
+print('end')
+
